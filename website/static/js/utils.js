e53724647d5f3646905f1c36ad594b2a4a0b1881
MIBismuth <jodulopes@gmail.com>
2024-12-26 13:26:10 +0000

    fix: content width, font-size

--- website/static/js/utils.js
+++ website/static/js/utils.js
@@ -13,14 +13,14 @@ function toggleTheme() {
     }
 }
 
-// /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-200px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+}
